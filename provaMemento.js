6511b90f77b9c14cec1ca5820d8db32a038f8046
sacroimper <masapim@hotmail.com>
2018-07-20 21:05:28 +0200

    Add crearFicha0EnCentro and vincularEntrada0 methods

--- provaMemento.js
+++ provaMemento.js
@@ -1,4 +1,355 @@
 
+// Literales
+const ANADIR_NUEVO_REGISTRO = 'AÑADIR NUEVO REGISTRO';
+
+
+
+
+// Librerias
+const LIB_CENTROS = 'Centros';
+const LIB_FICHA = 'Ficha';
+
+// Campos por libreria
+
+// Campos Centros
+const LIB_CENTROS_CAMPO_CODIGO_CLIENTE = 'Código cliente';
+const LIB_CENTROS_CAMPO_NOMBRE_COMERCIAL = 'Nombre Comercial';
+const LIB_CENTROS_CAMPO_RAZON_SOCIAL = 'Razón social';
+const LIB_CENTROS_CAMPO_DIRECCION = 'Dirección';
+const LIB_CENTROS_CAMPO_POBLACION = 'Población';
+const LIB_CENTROS_CAMPO_PROVINCIA = 'Província';
+const LIB_CENTROS_CAMPO_CODIGO_POSTAL = 'Código Postal';
+const LIB_CENTROS_CAMPO_TELEFONO = 'Teléfono';
+const LIB_CENTROS_CAMPO_MOVIL = 'Movil';
+const LIB_CENTROS_CAMPO_EMAIL = 'Email';
+const LIB_CENTROS_CAMPO_DELEGADO_ASIGNADO = 'Delegado asignado';
+const LIB_CENTROS_CAMPO_DIETISTA_ASIGNADA = 'Dietista asignada';
+const LIB_CENTROS_CAMPO_PERSONAS_DE_CONTACTO = 'Personas de contacto';
+const LIB_CENTROS_CAMPO_FICHA_CLIENTE = 'Ficha cliente';
+const LIB_CENTROS_CAMPO_IDCENTRO = 'idCentro';
+const LIB_CENTROS_CAMPO_IDDELEGADO = 'idDelegado';
+const LIB_CENTROS_CAMPO_IDDIETISTA = 'iddietista';
+
+
+// Campos Ficha
+const LIB_FICHA_CAMPO_IDPACIENTE = 'idPaciente';
+
+const LIB_FICHA_CAMPO_ACEPTACION = 'Aceptación';
+const LIB_FICHA_CAMPO_ACEPTADAS_CONDICIONES = 'Aceptadas condiciones';
+const LIB_FICHA_CAMPO_VER_DOCUMENTO_PROTECCION_DE_DATOS = 'Ver documento Protección de datos';
+const LIB_FICHA_CAMPO_QUIEN_LE_RECOMENDO_EL_TRATAMIENTO = 'Quien le recomendo el tratamiento';
+const LIB_FICHA_CAMPO_FECHA_ALTA = 'Fecha alta';
+const LIB_FICHA_CAMPO_OTROS = 'Otros';
+const LIB_FICHA_CAMPO_SITUACION = 'Situación';
+const LIB_FICHA_CAMPO_NOMBRE = 'Nombre';
+const LIB_FICHA_CAMPO_APELLIDOS = 'Apellidos';
+const LIB_FICHA_CAMPO_DNI = 'D.N.I.';
+const LIB_FICHA_CAMPO_SEXO = 'Sexo';
+const LIB_FICHA_CAMPO_FECHA_DE_NACIMIENTO = 'Fecha de nacimiento';
+const LIB_FICHA_CAMPO_DIRECCION = 'Dirección';
+const LIB_FICHA_CAMPO_CODIGO_POSTAL = 'Código Postal';
+const LIB_FICHA_CAMPO_POBLACION = 'Población';
+const LIB_FICHA_CAMPO_TELEFONO = 'Teléfono';
+const LIB_FICHA_CAMPO_MOVIL = 'Movil';
+const LIB_FICHA_CAMPO_EMAIL = 'Email';
+const LIB_FICHA_CAMPO_PATOLOGIAS_DIAGNOSTICADAS = 'Patologías diagnosticadas';
+const LIB_FICHA_CAMPO_OTRAS_PATOLOGIAS = 'Otras patologías';
+const LIB_FICHA_CAMPO_MEDICACION_OTRAS_PATOLOGIAS = 'Medicación Otras patologías';
+const LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_OTRAS_PATOLOGIAS = 'Tipo de medicación Otras patologías';
+const LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_OTRAS_PATOLOGIAS = 'Complementos alimenticios otras patologías';
+const LIB_FICHA_CAMPO_COM_OTRAS = 'com Otras';
+const LIB_FICHA_CAMPO_ARTICULAR = 'Articular';
+const LIB_FICHA_CAMPO_MEDICACION_ARTICULAR = 'Medicación Articular';
+const LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_ARTICULAR = 'Tipo de medicación Articular';
+const LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_ARTICULAR = 'Complementos alimenticios articular';
+const LIB_FICHA_CAMPO_COM_ARTICULAR = 'com Articular';
+const LIB_FICHA_CAMPO_CIRCULATORIA = 'Circulatoria';
+const LIB_FICHA_CAMPO_MEDICACION_CIRCULATORIA = 'Medicación Circulatoria';
+const LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_CIRCULATORIA = 'Tipo de medicación Circulatoria';
+const LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_CIRCULATORIA = 'Complementos alimenticios circulatoria';
+const LIB_FICHA_CAMPO_COM_CIRCULATORIA = 'com Circulatoria';
+const LIB_FICHA_CAMPO_CUTANEA = 'Cutanea';
+const LIB_FICHA_CAMPO_MEDICACION_CUTANEA = 'Medicación Cutanea';
+const LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_CUTANEO = 'Tipo de medicación Cutaneo';
+const LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_CUTANEA = 'Complementos alimenticios Cutanea';
+const LIB_FICHA_CAMPO_COM_CUTANEA = 'com Cutanea';
+const LIB_FICHA_CAMPO_DIGESTIVA = 'Digestiva';
+const LIB_FICHA_CAMPO_MEDICACION_DIGESTIVA = 'Medicación Digestiva';
+const LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_DIGESTIVA = 'Tipo de medicación Digestiva';
+const LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_DIGESTIVA = 'Complementos alimenticios Digestiva';
+const LIB_FICHA_CAMPO_COM_DIGESTIVA = 'com Digestiva';
+const LIB_FICHA_CAMPO_METABOLICA = 'Metabolica';
+const LIB_FICHA_CAMPO_MEDICACION_METABOLICA = 'Medicación Metabolica';
+const LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_METABOLICA = 'Tipo de medicación Metabolica';
+const LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_METABOLICA = 'Complementos alimenticios Metabolica';
+const LIB_FICHA_CAMPO_COM_METABOLICA = 'com Metabolica';
+const LIB_FICHA_CAMPO_RESPIRATORIA = 'Respiratoria';
+const LIB_FICHA_CAMPO_MEDICACION_RESPIRATORIA = 'Medicación Respiratoria';
+const LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_RESPIRATORIA = 'Tipo de medicación Respiratoria';
+const LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_RESPIRATORIA = 'Complementos alimenticios Respiratoria';
+const LIB_FICHA_CAMPO_COM_RESPIRATORIA = 'com Respiratoria';
+const LIB_FICHA_CAMPO_SISTEMA_NERVIOSO = 'Sistema nervioso';
+const LIB_FICHA_CAMPO_MEDICACION_SISTEMA_NERVIOSO = 'Medicación Sistema Nervioso';
+const LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_SISTEMA_NERVIOSO = 'Tipo de medicación Sistema Nervioso';
+const LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_SISTEMA_NERVIOSO = 'Complementos alimenticios Sistema Nervioso';
+const LIB_FICHA_CAMPO_COM_SISTEMA_NERVIOSO = 'com Sistema Nervioso';
+const LIB_FICHA_CAMPO_TRANSTORNOS_PSICOLOGICOS = 'Transtornos Psicológicos';
+const LIB_FICHA_CAMPO_MEDICACION_TRANSTORNOS_PSICOLOGICOS = 'Medicación Transtornos Psicológicos';
+const LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_TRANSTORNOS_PSICOLOGICOS = 'Tipo de medicación Transtornos Psicológicos';
+const LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_TRANSTORNOS_PSICOLOGICOS = 'Complementos alimenticios Transtornos Psicológicos';
+const LIB_FICHA_CAMPO_COM_TRANSTORNOS_PSICOLOGICOS = 'com Transtornos Psicológicos';
+const LIB_FICHA_CAMPO_INTERVENCIONES_QUIRURJICAS = 'Intervenciones Quirurjicas';
+const LIB_FICHA_CAMPO_PRESION_ARTERIAL = 'Presión arterial';
+const LIB_FICHA_CAMPO_MEDICIONES_PRESION_ARTERIAL = 'Mediciones Presión arterial';
+const LIB_FICHA_CAMPO_COLESTEROL = 'Colesterol';
+const LIB_FICHA_CAMPO_HDL = 'HDL';
+const LIB_FICHA_CAMPO_LDL = 'LDL';
+const LIB_FICHA_CAMPO_COLESTEROL_TOTAL = 'Colesterol total';
+const LIB_FICHA_CAMPO_TRIGLICERIDOS = 'Triglicéridos';
+const LIB_FICHA_CAMPO_AÑADIR_INFORMACION_DE_OTROS_DATOS_ANALITICOS = 'Añadir Información de otros datos analíticos';
+const LIB_FICHA_CAMPO_MOSTRAR_ANALITICA = 'Mostrar Analitica';
+const LIB_FICHA_CAMPO_IMG_ANALITICA = 'Img analitica';
+const LIB_FICHA_CAMPO_ALERGIAS = 'Alergias';
+const LIB_FICHA_CAMPO_ALERGIAS__ALIMENTICIAS = 'Alergias  Alimenticias';
+const LIB_FICHA_CAMPO_ALERGIAS_MEDICAMENTOSAS = 'Alergias Medicamentosas';
+const LIB_FICHA_CAMPO_ALERGIAS_OTRAS = 'Alergias Otras';
+const LIB_FICHA_CAMPO_INTOLERANCIAS_ALIMENTICIAS = 'Intolerancias alimenticias';
+const LIB_FICHA_CAMPO_OTRAS_INTOLERANCIAS = 'Otras Intolerancias';
+const LIB_FICHA_CAMPO_CAMINAR = 'Caminar';
+const LIB_FICHA_CAMPO_DEPORTES = 'Deportes';
+const LIB_FICHA_CAMPO_DEPORTE_QUE_PRACTICA = 'Deporte que practica';
+const LIB_FICHA_CAMPO_DESAYUNO = 'Desayuno';
+const LIB_FICHA_CAMPO_HORA_DESAYUNO = 'Hora desayuno';
+const LIB_FICHA_CAMPO_MÈDIA_MAÑANA = 'Mèdia mañana';
+const LIB_FICHA_CAMPO_HORARIO_MEDIA_MAÑANA = 'Horario media mañana';
+const LIB_FICHA_CAMPO_COMIDA = 'Comida';
+const LIB_FICHA_CAMPO_HORA_COMIDA = 'Hora comida';
+const LIB_FICHA_CAMPO_MERIENDA = 'Merienda';
+const LIB_FICHA_CAMPO_HORA_MERIENDA = 'Hora merienda';
+const LIB_FICHA_CAMPO_CENA = 'Cena';
+const LIB_FICHA_CAMPO_HORA_CENA = 'Hora cena';
+const LIB_FICHA_CAMPO_CONSUMO_DE_LACTEOS = 'Consumo de Lacteos';
+const LIB_FICHA_CAMPO_CONSUMO_DE_FRUTA = 'Consumo de Fruta';
+const LIB_FICHA_CAMPO_CONSUMO_DE_LEGUMBRES = 'Consumo de Legumbres';
+const LIB_FICHA_CAMPO_CONSUMO_DE_PASTA = 'Consumo de Pasta';
+const LIB_FICHA_CAMPO_CONSUMO_DE_ARROZ = 'Consumo de Arroz';
+const LIB_FICHA_CAMPO_CONSUMO_DE_VERDURA = 'Consumo de Verdura';
+const LIB_FICHA_CAMPO_CONSUMO_DE_ENSALADA = 'Consumo de Ensalada';
+const LIB_FICHA_CAMPO_CONSUMO_DE_CARNE = 'Consumo de Carne';
+const LIB_FICHA_CAMPO_CONSUMO_DE_PESCADO = 'Consumo de Pescado';
+const LIB_FICHA_CAMPO_CONSUMO_DIARIO_DE_AGUA = 'Consumo diario de agua';
+const LIB_FICHA_CAMPO_INTENTAR_NO_INCLUIR_EN_LAS_DIETAS = 'Intentar no incluir en las dietas';
+const LIB_FICHA_CAMPO_ANSIEDAD = 'Ansiedad';
+const LIB_FICHA_CAMPO_TIPO_DE_ANSIEDAD = 'Tipo de Ansiedad';
+const LIB_FICHA_CAMPO_ANSIEDAD_OTROS = 'Ansiedad Otros';
+const LIB_FICHA_CAMPO_EN_QUE_MOMENTO_DEL_DIA_SE_PRODUCTE_LA_ANSIEDAD = 'En que momento del día se producte la ansiedad';
+const LIB_FICHA_CAMPO_WC = 'WC';
+const LIB_FICHA_CAMPO_WC_OTROS = 'WC Otros';
+const LIB_FICHA_CAMPO_REGLAS = 'Reglas';
+const LIB_FICHA_CAMPO_REGLAS_OTRAS = 'Reglas Otras';
+const LIB_FICHA_CAMPO_ALTURA = 'Altura';
+const LIB_FICHA_CAMPO_VISITA = 'Visita';
+const LIB_FICHA_CAMPO_HISTORICO = 'Histórico';
+const LIB_FICHA_CAMPO_IDDIETISTA = 'iddietista';
+const LIB_FICHA_CAMPO_IDCENTRO = 'idCentro';
+const LIB_FICHA_CAMPO_FILTRO = 'filtro';
+
+// Array campos copiables ficha
+var CAMPOS_FICHA = {
+    LIB_FICHA_CAMPO_ACEPTACION, LIB_FICHA_CAMPO_ACEPTADAS_CONDICIONES, LIB_FICHA_CAMPO_VER_DOCUMENTO_PROTECCION_DE_DATOS, LIB_FICHA_CAMPO_QUIEN_LE_RECOMENDO_EL_TRATAMIENTO, LIB_FICHA_CAMPO_FECHA_ALTA, LIB_FICHA_CAMPO_OTROS, LIB_FICHA_CAMPO_SITUACION, LIB_FICHA_CAMPO_NOMBRE, LIB_FICHA_CAMPO_APELLIDOS, LIB_FICHA_CAMPO_DNI, LIB_FICHA_CAMPO_SEXO, LIB_FICHA_CAMPO_FECHA_DE_NACIMIENTO, LIB_FICHA_CAMPO_DIRECCION, LIB_FICHA_CAMPO_CODIGO_POSTAL, LIB_FICHA_CAMPO_POBLACION, LIB_FICHA_CAMPO_TELEFONO, LIB_FICHA_CAMPO_MOVIL, LIB_FICHA_CAMPO_EMAIL, LIB_FICHA_CAMPO_PATOLOGIAS_DIAGNOSTICADAS, LIB_FICHA_CAMPO_OTRAS_PATOLOGIAS, LIB_FICHA_CAMPO_MEDICACION_OTRAS_PATOLOGIAS, LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_OTRAS_PATOLOGIAS, LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_OTRAS_PATOLOGIAS, LIB_FICHA_CAMPO_COM_OTRAS, LIB_FICHA_CAMPO_ARTICULAR, LIB_FICHA_CAMPO_MEDICACION_ARTICULAR, LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_ARTICULAR, LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_ARTICULAR, LIB_FICHA_CAMPO_COM_ARTICULAR, LIB_FICHA_CAMPO_CIRCULATORIA, LIB_FICHA_CAMPO_MEDICACION_CIRCULATORIA, LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_CIRCULATORIA, LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_CIRCULATORIA, LIB_FICHA_CAMPO_COM_CIRCULATORIA, LIB_FICHA_CAMPO_CUTANEA, LIB_FICHA_CAMPO_MEDICACION_CUTANEA, LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_CUTANEO, LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_CUTANEA, LIB_FICHA_CAMPO_COM_CUTANEA, LIB_FICHA_CAMPO_DIGESTIVA, LIB_FICHA_CAMPO_MEDICACION_DIGESTIVA, LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_DIGESTIVA, LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_DIGESTIVA, LIB_FICHA_CAMPO_COM_DIGESTIVA, LIB_FICHA_CAMPO_METABOLICA, LIB_FICHA_CAMPO_MEDICACION_METABOLICA, LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_METABOLICA, LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_METABOLICA, LIB_FICHA_CAMPO_COM_METABOLICA, LIB_FICHA_CAMPO_RESPIRATORIA, LIB_FICHA_CAMPO_MEDICACION_RESPIRATORIA, LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_RESPIRATORIA, LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_RESPIRATORIA, LIB_FICHA_CAMPO_COM_RESPIRATORIA, LIB_FICHA_CAMPO_SISTEMA_NERVIOSO, LIB_FICHA_CAMPO_MEDICACION_SISTEMA_NERVIOSO, LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_SISTEMA_NERVIOSO, LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_SISTEMA_NERVIOSO, LIB_FICHA_CAMPO_COM_SISTEMA_NERVIOSO, LIB_FICHA_CAMPO_TRANSTORNOS_PSICOLOGICOS, LIB_FICHA_CAMPO_MEDICACION_TRANSTORNOS_PSICOLOGICOS, LIB_FICHA_CAMPO_TIPO_DE_MEDICACION_TRANSTORNOS_PSICOLOGICOS, LIB_FICHA_CAMPO_COMPLEMENTOS_ALIMENTICIOS_TRANSTORNOS_PSICOLOGICOS, LIB_FICHA_CAMPO_COM_TRANSTORNOS_PSICOLOGICOS, LIB_FICHA_CAMPO_INTERVENCIONES_QUIRURJICAS, LIB_FICHA_CAMPO_PRESION_ARTERIAL, LIB_FICHA_CAMPO_MEDICIONES_PRESION_ARTERIAL, LIB_FICHA_CAMPO_COLESTEROL, LIB_FICHA_CAMPO_HDL, LIB_FICHA_CAMPO_LDL, LIB_FICHA_CAMPO_COLESTEROL_TOTAL, LIB_FICHA_CAMPO_TRIGLICERIDOS, LIB_FICHA_CAMPO_AÑADIR_INFORMACION_DE_OTROS_DATOS_ANALITICOS, LIB_FICHA_CAMPO_MOSTRAR_ANALITICA, LIB_FICHA_CAMPO_IMG_ANALITICA, LIB_FICHA_CAMPO_ALERGIAS, LIB_FICHA_CAMPO_ALERGIAS__ALIMENTICIAS, LIB_FICHA_CAMPO_ALERGIAS_MEDICAMENTOSAS, LIB_FICHA_CAMPO_ALERGIAS_OTRAS, LIB_FICHA_CAMPO_INTOLERANCIAS_ALIMENTICIAS, LIB_FICHA_CAMPO_OTRAS_INTOLERANCIAS, LIB_FICHA_CAMPO_CAMINAR, LIB_FICHA_CAMPO_DEPORTES, LIB_FICHA_CAMPO_DEPORTE_QUE_PRACTICA, LIB_FICHA_CAMPO_DESAYUNO, LIB_FICHA_CAMPO_HORA_DESAYUNO, LIB_FICHA_CAMPO_MÈDIA_MAÑANA, LIB_FICHA_CAMPO_HORARIO_MEDIA_MAÑANA, LIB_FICHA_CAMPO_COMIDA, LIB_FICHA_CAMPO_HORA_COMIDA, LIB_FICHA_CAMPO_MERIENDA, LIB_FICHA_CAMPO_HORA_MERIENDA, LIB_FICHA_CAMPO_CENA, LIB_FICHA_CAMPO_HORA_CENA, LIB_FICHA_CAMPO_CONSUMO_DE_LACTEOS, LIB_FICHA_CAMPO_CONSUMO_DE_FRUTA, LIB_FICHA_CAMPO_CONSUMO_DE_LEGUMBRES, LIB_FICHA_CAMPO_CONSUMO_DE_PASTA, LIB_FICHA_CAMPO_CONSUMO_DE_ARROZ, LIB_FICHA_CAMPO_CONSUMO_DE_VERDURA, LIB_FICHA_CAMPO_CONSUMO_DE_ENSALADA, LIB_FICHA_CAMPO_CONSUMO_DE_CARNE, LIB_FICHA_CAMPO_CONSUMO_DE_PESCADO, LIB_FICHA_CAMPO_CONSUMO_DIARIO_DE_AGUA, LIB_FICHA_CAMPO_INTENTAR_NO_INCLUIR_EN_LAS_DIETAS, LIB_FICHA_CAMPO_ANSIEDAD, LIB_FICHA_CAMPO_TIPO_DE_ANSIEDAD, LIB_FICHA_CAMPO_ANSIEDAD_OTROS, LIB_FICHA_CAMPO_EN_QUE_MOMENTO_DEL_DIA_SE_PRODUCTE_LA_ANSIEDAD, LIB_FICHA_CAMPO_WC, LIB_FICHA_CAMPO_WC_OTROS, LIB_FICHA_CAMPO_REGLAS, LIB_FICHA_CAMPO_REGLAS_OTRAS, LIB_FICHA_CAMPO_ALTURA, LIB_FICHA_CAMPO_VISITA, LIB_FICHA_CAMPO_HISTORICO, LIB_FICHA_CAMPO_IDDIETISTA, LIB_FICHA_CAMPO_IDCENTRO, LIB_FICHA_CAMPO_FILTRO
+};
+
+
+function libreria(libreria) {
+    if (typeof libreria == 'string') {
+        libreria = lib(libreria);
+    }
+    return libreria;
+}
+
+function forEach(coleccion, funcion) {
+    for (let i = 0; i < coleccion.length; i++) {
+        funcion(coleccion[i], i);
+    }
+}
+
+
+function crearFicha0EnCentro(centro) {
+    var libreriaDestino = LIB_FICHA;
+    var campoLinks = LIB_CENTROS_CAMPO_FICHA_CLIENTE;
+    var campoLinkDestino = LIB_FICHA_CAMPO_IDCENTRO;
+    var campoMaestro = LIB_CENTROS_CAMPO_IDDIETISTA;
+    var campoMaestroDestino = LIB_FICHA_CAMPO_IDDIETISTA;
+    var campoIdDestino = LIB_FICHA_CAMPO_IDPACIENTE;
+    var objetoEntrada0 = {};
+
+    objetoEntrada0[LIB_FICHA_CAMPO_IDPACIENTE] = 0;
+    objetoEntrada0[LIB_FICHA_CAMPO_SITUACION] = ANADIR_NUEVO_REGISTRO;
+
+    var funcionCrearEntrada0 = null;
+
+    vincularEntrada0(centro, libreriaDestino, campoLinks, campoLinkDestino, campoMaestro, campoMaestroDestino, campoIdDestino, objetoEntrada0, funcionCrearEntrada0);
+}
+
+/**
+ * 
+ * @param {Entry} entradaActual Entrada actual, entry() si es null
+ * @param {String|Lib} libreriaDestino Libreria donde esta la entrada 0 (nombre o objeto)
+ * @param {String} campoLinks Campo de la entrada actual donde debe estar el link a la entrada 0
+ * @param {String} campoLinkDestino Campo de la entrada 0 donde debe estar el link a la entrada actual
+ * @param {String} campoMaestro Campo de la entrada actual que determina que entrada 0 debo buscar
+ * @param {String} campoMaestroDestino Campo de la entrada 0 que determina que entrada 0 debo buscar
+ * @param {String} campoIdDestino Campo identificador de la entrada destino
+ * @param {Object} objetoEntrada0 Objeto con los campo/valores de la nueva entrada 0
+ * @param {Function} objetoEntrada0 Funcion para completar la nueva entrada 0
+ */
+function vincularEntrada0(entradaActual, libreriaDestino, campoLinks, campoLinkDestino, campoMaestro, campoMaestroDestino, campoIdDestino, objetoEntrada0, funcionCrearEntrada0) {
+    if (entradaActual == null) {
+        entradaActual = entry();
+    }
+    libreriaDestino = libreria(libreriaDestino);
+
+    var links = entradaActual.field(campoLinks);
+    var contieneEntrada0 = false;
+
+    var entradaMaestro = entradaActual.field(campoMaestro)[0]
+
+    var entradasDestino = libreriaDestino.linksTo(entradaMaestro);
+    var entrada0 = null;
+
+    forEach(entradasDestino, function (entradaDestino, i) {
+        if (entradaDestino.field(campoIdDestino) == 0) {
+            entrada0 = entradaDestino;
+        }
+    });
+
+    if (entrada0 != null) {
+        forEach(links, function (link, i) {
+            if (link.field(campoIdDestino) == 0) {
+                if (link.id == entrada0.id) {
+                    contieneEntrada0 = true;
+                    message('Contiene');
+                } else {
+                    entradaActual.unlink(campoLinks, link);
+                }
+            }
+        });
+    }
+
+    if (!contieneEntrada0) {
+        message('No contiene');
+
+        if (entrada0 == null) {
+
+            entrada0 = libreriaDestino.create(objetoEntrada0);
+
+            entrada0.link(campoMaestroDestino, entradaMaestro);
+
+            if (funcionCrearEntrada0 != null) {
+                funcionCrearEntrada0(entradaActual, entrada0, entradaMaestro);
+            }
+        }
+
+        var linksDestino = entrada0.field(campoLinkDestino);
+
+        forEach(linksDestino, function (linkDestino, i) {
+            entrada0.unlink(campoLinkDestino, linkDestino);
+        });
+
+        entrada0.link(campoLinkDestino, entradaActual);
+        entrada0.recalc();
+
+        entradaActual.link(campoLinks, entrada0);
+        entradaActual.recalc();
+
+    }
+}
+
+
+// function crearFicha0EnCentro() {
+
+//     var centro = entry();
+//     var libFichas = libByName('Ficha');
+
+//     var fichasCentro = centro.field('Ficha cliente');
+//     var contieneFicha0 = false;
+
+//     var fichasDietista = libFichas.linksTo(centro.field('iddietista')[0]);
+//     var ficha0 = null;
+//     for (var i = 0; i < fichasDietista.length; i++) {
+//         if (fichasDietista[i].field('idPaciente') == 0) {
+//             ficha0 = fichasDietista[i];
+//         }
+//     }
+
+//     if (ficha0 != null) {
+//         for (var i = 0; i < fichasCentro.length && !contieneFicha0; i++) {
+//             var ficha = fichasCentro[i];
+//             if (ficha.field('idPaciente') == 0) {
+
+//                 if (ficha.id == ficha0.id) {
+//                     contieneFicha0 = true;
+//                     message('Contiene');
+//                 } else {
+//                     centro.unlink('Ficha cliente', ficha);
+//                 }
+//             }
+//         }
+//     }
+
+//     if (!contieneFicha0) {
+//         message('No contiene');
+
+
+//         if (ficha0 == null) {
+//             var tmpFicha0 = {};
+
+//             tmpFicha0['idPaciente'] = 0;
+//             //tmpFicha0['Nombre'] = 'AÑADIR NUEVO REGISTRO';
+//             tmpFicha0['Situación'] = 'NUEVO REGISTRO';
+
+//             ficha0 = libFichas.create(tmpFicha0);
+
+//             ficha0.link('iddietista', centro.field('iddietista')[0]);
+
+
+//         }
+
+//         var centrosFicha = ficha0.field('idCentro');
+//         for (var j = 0; j < centrosFicha.length; j++) {
+//             ficha0.unlink('idCentro', centrosFicha[j]);
+//         }
+//         ficha0.link('idCentro', centro);
+//         ficha0.recalc();
+
+//         centro.link('Ficha cliente', ficha0);
+//         centro.recalc();
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function helloWorld() {
 
     message('Hello World!!');
